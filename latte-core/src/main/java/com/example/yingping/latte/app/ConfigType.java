bba3db40370e8a15cf53394f70c956838623009c
yingping <907198993@qq.com>
2017-11-18 16:59:07 +0800

    add latte core

---
+++ latte-core/src/main/java/com/example/yingping/latte/app/ConfigType.java
@@ -0,0 +1,12 @@
+package com.example.yingping.latte.app;
+
+/**
+ * Created by yingping on 2017/11/18.
+ */
+//唯一的单例，只初始化一次
+public enum ConfigType {
+    API_HOST,
+    APPLICATION_CONTEXT,
+    CONFIG_READY,
+    ICON
+}
