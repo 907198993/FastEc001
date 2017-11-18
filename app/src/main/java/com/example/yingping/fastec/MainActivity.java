76bed03a2e871496459e6eb6133aadffbb173900
yingping <907198993@qq.com>
2017-11-18 13:43:37 +0800

    3 commit

--- app/src/main/java/com/example/yingping/fastec/MainActivity.java
+++ app/src/main/java/com/example/yingping/fastec/MainActivity.java
@@ -8,7 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
- ///123123123
         setContentView(R.layout.activity_main);
     }
 }
